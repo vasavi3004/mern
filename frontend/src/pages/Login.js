40255d0c9a7e7451d43e1714f05a86e58dd4447c
Vasavi <184518108+vasavi3004@users.noreply.github.com>
2025-11-02 19:35:26 +0530

    Update login API endpoint URL

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://mern-backend-lh4u.onrender.com/api/auth/login", {
+      const res = await axios.post("https://mern-qy2v.onrender.com/api/auth/login", {
         email,
         password,
       });
